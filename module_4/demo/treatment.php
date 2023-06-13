3e85f92d30f5c3c84a243735fdcf3b3cce14d734
stropee <stropee@campus-eni.fr>
2023-06-13 16:41:40 +0200

    Form demo 1

--- module_4/demo/treatment.php
+++ module_4/demo/treatment.php
@@ -3,7 +3,15 @@
 var_dump($_REQUEST);
 
 if(isset($_REQUEST['submit'])){
-    echo "Le login est " . $_REQUEST['login'];
+
+    //$login = htmlspecialchars($_REQUEST['login']);
+    $login = filter_var($_REQUEST['login'], FILTER_SANITIZE_SPECIAL_CHARS);
+    $password = filter_var($_REQUEST['password'], FILTER_SANITIZE_STRING);
+
+
+    var_dump($login);
+    var_dump($password);
+
 }else{
     echo 'Utilisateur non connecté !';
 }
